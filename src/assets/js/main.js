442fb2b7f17ba533f560d5ba41cbae125d7084d2
DESKTOP-G7I7M5O\kape9 <angelcapellaruiz@gmail.com>
2020-10-29 09:52:17 +0100

    0.1.2

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,22 @@
+const text = document.getElementById('text');
+const formulario = document.getElementById('formulario');
+const confirmacion = document.getElementById('confirmacion');
+const mensaje = document.getElementById('mensaje');
+const url = document.getElementById('url');
+
+
 function ocultar(){
     document.getElementById('text').style.display = 'none';
-   
+    document.getElementById('mensaje').style.display = 'none';
+    document.getElementById('confirmacion').style.display = 'none';
 }
+
+
+
+
+
+
+
 
 
 
