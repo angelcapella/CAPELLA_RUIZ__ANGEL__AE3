e1add01585fbbd7f5dc97861c3e75f819d207bca
DESKTOP-G7I7M5O\kape9 <angelcapellaruiz@gmail.com>
2020-11-02 22:27:24 +0100

    1.1

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', () => {
         arrayGuardar.forEach(element=>{
             html = html + element + '<br>'
         });
-        getElementById('pintar').innerHTML = html;
+        document.getElementById('pintar').innerHTML = html;
     }
 
 
