41bc6cd2f7664aea8faced06f8b6aeca7bb48e4e
DESKTOP-G7I7M5O\kape9 <angelcapellaruiz@gmail.com>
2020-11-02 19:57:08 +0100

    1.0

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,25 +1,56 @@
-const text = document.getElementById('text');
-const formulario = document.getElementById('formulario');
-const confirmacion = document.getElementById('confirmacion');
-const mensaje = document.getElementById('mensaje');
-const url = document.getElementById('url');
+let urlf = ''
+document.addEventListener('DOMContentLoaded', () => {
+    siguiente = (numero,id,requerido) =>{
+        if(requerido){
+            if(document.getElementById(id).value > 0){
+                document.getElementById('g'+(numero-1)).classList.add('displaynone');
+                document.getElementById('g'+numero).classList.remove('displaynone');
+                
+                quitarError();
+            }else{
+                
+                saltarError();
+            }
+        }else{
+            document.getElementById('g'+(numero-1)).classList.add('displaynone');
+            document.getElementById('g'+numero).classList.remove('displaynone');
+        }
+    }
+
+    retroceder = (numero) =>{
+        document.getElementById('g'+(numero-1)).classList.remove('displaynone');
+        document.getElementById('g'+numero).classList.add('displaynone');
+    }
+
+    Coger = () =>{
+        const form = document.getElementsByClassName('form')[0]
+        let arrayGuardar = [];         
+        for (let i = 0; i < form.length; i++) {    
+            arrayGuardar[i] = form[i].value;  
+        }
+        console.log(arrayGuardar);  
+        let html = '';
+        arrayGuardar.forEach(element=>{
+            html = html + element + '<br>'
+        });
+        getElementById('pintar').innerHTML = html;
+    }
 
 
-function ocultar(){
-    document.getElementById('text').style.visibility = 'hidden';
-    document.getElementById('mensaje').style.visibility = 'hidden';
-    document.getElementById('confirmacion').style.visibility = 'hidden';
-}
-
-function volver() {
- document.getElementById('formulario').style.visibility = 'hidden';   
-}
+    enviarurlf = () =>{
+        console.log("se mete la url",document.getElementById('urltodo').value);
+        document.getElementById('urlalmacen').innerHTML = document.getElementById('urltodo').value;
+        console.log("urlalmacen",document.getElementById('urlalmacen').innerHTML);
+        siguiente(2);
+    }
 
 
-
-
-
-
+    terminartodo = () =>{
+        console.log("url abriendose",document.getElementById('urlalmacen').textContent);
+        window.open(document.getElementById('urlalmacen').textContent);
+    }
+    
+});
 
 
 
